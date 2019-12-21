3a2cf40e05bd08ab126a0edbbe75bdc8acc1687a
Nikolay Popov <koyabica@gmail.com>
2019-12-21 17:48:23 +0300

    feature/reset password

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from hashlib import md5
 from datetime import datetime
-from app import db
+from time import time
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin 
-from app import login
+from app import app, db, login
+from flask_login import UserMixin
+
 
 
 @login.user_loader
@@ -31,11 +33,27 @@ class User(UserMixin, db.Model):
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 
+
 	def set_password(self, password):
 		self.password_hash = generate_password_hash(password)
 
 	def check_password(self, password):
 		return check_password_hash(self.password_hash, password)
+
+	def get_reset_password_token(self, expires_in=600):
+		return jwt.encode(
+			{'reset_password': self.id, 'exp': time() + expires_in},
+			app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+	@staticmethod
+	def verify_reset_password_token(token):
+		try:
+			id = jwt.decode(token, app.config['SECRET_KEY'],
+							algorithms=['HS256'])['reset_password']
+		except:
+			return
+		return User.query.get(id)
+
 
 	def avatar(self, size):
 		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
